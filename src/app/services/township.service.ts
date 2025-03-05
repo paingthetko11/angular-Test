9c42e0280ff0ad2a6f81c01f3666f159db472b84
paingthetko11 <paingthetko081@gmail.com>
2025-03-05 17:04:35 +0630

    update code

---
+++ src/app/services/township.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RootModel } from '../models/root.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TownshipServices {
+
+   constructor(private http: HttpClient) {}
+
+   getjson(): Observable<RootModel> {
+     let url: string = 'https://localhost:7010/api/Township';
+     return this.http.get<RootModel>(url);
+   }
+}
