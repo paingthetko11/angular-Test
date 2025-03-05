770a1247a20bb21730e234d837e0af0b676f2c92
paingthetko11 <paingthetko081@gmail.com>
2025-03-05 17:03:57 +0630

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { StreetComponent } from './street/street.component';
 import { ButtomComponent } from './buttom/buttom.component';
 import { SelectComponent } from './select/select.component';
+import { TownshipComponent } from './township/township.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: "full" },
     { path: 'home', component: HomeComponent },
     { path: 'street', component: StreetComponent },
     {path:'buttom', component:ButtomComponent},
-    {path:"select",component:SelectComponent}
+    {path:"select",component:SelectComponent},
+    {path:'township',component:TownshipComponent}
 ];
